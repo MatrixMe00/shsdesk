eb86893910563cea386af09b54925179868ea095
MatrixMe00 <safosah00@gmail.com>
2023-02-11 05:26:55 +0000

    Nav UI change

--- blocks/nav.php
+++ blocks/nav.php
@@ -1,31 +1,39 @@
-<nav>
+<nav class="sp-xlg-tp">
         <div id="logo">
             <div id="name">
                 <span id="first">SHS</span>
                 <span id="last">Desk</span>
             </div>
         </div>
+        <?php 
+            $links = array();
+            $ur = $_SERVER["REQUEST_URI"];
+
+            if(strpos($ur, "shsdesk")){
+                $links = array(
+                    [ "url" => "/", "name" => "Home"],
+                    [ "url" => "/pages/about.php", "name" => "About"],
+                    [ "url" => "/pages/school.php", "name" => "Schools"],
+                    // [ "url" => "/pages/faq.php", "name" => "FAQ"],
+                    [ "url" => "/pages/contact.php", "name" => "Contact"]
+                );
+            }else{
+                $links = array(
+                    [ "url" => "/", "name" => "Home"],
+                    [ "url" => "/about", "name" => "About"],
+                    [ "url" => "/school", "name" => "Schools"],
+                    // [ "url" => "/faq", "name" => "FAQ"],
+                    [ "url" => "/contact", "name" => "Contact"]
+                );
+            }
+            
+        ?>
         <div id="buttons">
-            <a href="<?php echo $url?>/index.php" class="button">
-                <span>Home</span>
+            <?php foreach($links as $link) : ?>
+            <a href="<?php echo $url.$link["url"]?>" class="button">
+                <span><?= $link["name"] ?></span>
             </a>
-            <a href="<?php echo $url?>/pages/about.php" class="button">
-                <span>About</span>
-            </a>
-            <a href="<?php echo $url?>/pages/school.php" class="button">
-                <span>Schools</span>
-            </a>
-            <?php
-            $show = false;
-            if($show){
-            ?>
-            <a href="<?php echo $url?>/pages/faq.php" class="button">
-                <span>FAQ</span>
-            </a>
-            <?php } ?>
-            <a href="<?php echo $url?>/pages/contact.php" class="button">
-                <span>Contact Us</span>
-            </a>
+            <?php endforeach; ?>
             <a href="<?php echo $url?>/student/" class="button">
                 <span>Student Menu</span>
             </a>
@@ -36,4 +44,5 @@
             <span></span>
         </div>
     </nav>
+    <?php $show = false ?>
 
