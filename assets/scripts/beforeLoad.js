25f1e3de9ab859a0c67b5265cd8d4d6ae811c0e3
MatrixMe00 <safosah00@gmail.com>
2023-02-13 03:01:11 +0000

    Modify loaders

--- assets/scripts/beforeLoad.js
+++ assets/scripts/beforeLoad.js
@@ -1,12 +1,19 @@
-const loader = "<div id=\"page_loading\" class=\"absolute center txt-fl white\" style=\"z-index: 900\">" + 
+//the loader container
+const loader = "<div id=\"page_loading\" class=\"absolute center txt-fl\" style=\"z-index: 900; background-color: white\">" + 
                 "<span>Page is loading <span id=\"circle-animate\" class=\"black sp-sm round\" style=\"width: 10px; height: 10px; display: inline-block\"></span></span>" + 
                 "</div>"
 
 //push loader into body
 $("html").append(loader)
 $("body").css("overflow", "hidden")
+
+//opacity for the circle
 opacity = 0;
 
+//let animation display at least once
+run_once = true;
+
+//blinking animation using interval
 blink = setInterval(function(){
     opacity = opacity == 0 ? 1 : 0
     $("#circle-animate").css("opacity", opacity)
@@ -14,10 +21,13 @@ blink = setInterval(function(){
 }, 500)
 
 timer = setInterval(function(){
-    if($("body") && $("body").html() != ""){
+    if($("body") && $("body").html() != "" && !run_once){
         clearInterval(timer)
         clearInterval(blink)
         $("#page_loading").remove();
         $("body").css("overflow", "auto")
     }
+
+    //declare that it has finished running once
+    run_once = false
 }, 2000)
