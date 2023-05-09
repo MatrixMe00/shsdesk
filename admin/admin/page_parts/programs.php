12ddd6452878c3af083e2380186cc57fa9838bd1
MatrixMe00 <safosah00@gmail.com>
2023-05-09 08:28:16 +0000

    Update programs.php

--- admin/admin/page_parts/programs.php
+++ admin/admin/page_parts/programs.php
@@ -171,7 +171,7 @@
 </section>
 
 <section id="newProgram" class="sp-xlg-tp section_box no_disp">
-    <?php require "$url/admin/admin/page_parts/subTeaForms.php?form_type=addProgram&school_id=$user_school_id" ?>
+    <?php eval("?>".file_get_contents("$url/admin/admin/page_parts/subTeaForms.php?form_type=addProgram&school_id=$user_school_id")) ?>
 </section>
 
 <div id="table_del" class="modal_yes_no fixed flex flex-center-content flex-center-align form_modal_box no_disp">
@@ -183,7 +183,7 @@
         <div id="getLoader"></div>
         <span class="item-event" id="cancelUpdate" style="color: white; margin-top: 10px; padding-left: 10px; text-align: center">Cancel</span>
     </div>
-    <?php require "$url/admin/admin/page_parts/subTeaForms.php?form_type=updateProgram&school_id=$user_school_id" ?>
+    <?php eval("?>".file_get_contents("$url/admin/admin/page_parts/subTeaForms.php?form_type=updateProgram&school_id=$user_school_id")) ?>
 </div>
 
 <script>
