7d7039392f429ecffe8055a69c9b473fdd121702
MatrixMe00 <safosah00@gmail.com>
2023-05-17 17:28:32 +0000

    Update dashboard.php
    Dashboard has been connected to the backend

--- teacher/components/dashboard.php
+++ teacher/components/dashboard.php
@@ -1,15 +1,24 @@
-<?php include_once("compSession.php"); $_SESSION["active-page"] = "dashboard" ?><section class="d-section lt-shade">
+<?php 
+    include_once("compSession.php"); 
+    $_SESSION["active-page"] = "dashboard";
+
+    //useful variables in this page
+    $course_ids = explode(" ", $teacher["course_id"]);
+    $program_ids = explode(" ", $teacher["program_ids"]);
+    $result_type = fetchData("school_result","admissiondetails","schoolID=".$teacher["school_id"])["school_result"];
+?>
+<section class="d-section lt-shade">
     <div class="img txt-al-c">
         <img src="<?= "$mainRoot/assets/images/icons/city hall.png" ?>" class="rect-xsm" alt="school logo">
     </div>
     <div class="time flex flex-column gap-md txt-al-c">
         <div class="flex flex-space-content txt-fs">
-            <span>Current Year: 2021/2022</span>
+            <span>Current Year: <?= fetchData("academicYear","admissiondetails","schoolID=".$teacher["school_id"])["academicYear"] ?></span>
             <span>Current Term: 1</span>
         </div>
         <div class="flex flex-column">
             <div id="cur_time" class="txt-fl3 txt-bold"></div>
-            <div id="date" class="txt-fs">May 14, 2023</div>
+            <div id="date" class="txt-fs"><?= date("F d, Y") ?></div>
         </div>
     </div>
 </section>
@@ -17,11 +26,21 @@
 <section class="flex d-section flex-wrap gap-sm p-lg card-section">
     <div class="card v-card gap-lg purple p-med sm-rnd flex-wrap">
         <span class="">Students</span>
-        <span class="txt-fl3 txt-bold">20</span>
+        <span class="txt-fl3 txt-bold">
+            <?php             
+                $sql = "SELECT COUNT(s.indexNumber) AS total
+                    FROM students_table s
+                    JOIN program p ON s.program_id = p.program_id
+                    JOIN teachers t ON p.school_id = t.school_id
+                    WHERE CONCAT(p.program_id, ' ') IN (" . implode(' ', $program_ids) . ")";
+                $query = $connect2->query($sql);
+                echo $query->fetch_assoc()["total"];
+            ?>
+        </span>
     </div>
     <div class="card v-card gap-lg orange p-med sm-rnd flex-wrap">
         <span class="">Class Count</span>
-        <span class="txt-fl3 txt-bold"><?= $max = rand(1,10) ?></span>
+        <span class="txt-fl3 txt-bold"><?= count($program_ids)-1 ?></span>
     </div>
 </section>
 
@@ -41,20 +60,34 @@
             </div>
         </div>
         <div class="body flex flex-wrap m-sm">
-            <?php for($i=1; $i <= $max; $i++) :
-                $grade = giveGrade(rand(50, 95));
-                $total = rand(20,50);
+            <?php 
+                $sql = "SELECT p.program_id, p.program_name, p.short_form as short_p, c.course_name, c.course_id, c.short_form as short_c
+                    FROM courses c
+                    JOIN program p ON p.school_id = c.school_id
+                    WHERE ('".implode(' ',$course_ids)."' LIKE CONCAT('%',c.course_id, ' ', '%')) AND ('".implode(" ",$program_ids)."' LIKE CONCAT('%',p.program_id, ' ', '%'))";
+                $query = $connect2->query($sql);
+                if($query->num_rows > 0) : 
+                    while($class = $query->fetch_assoc()) :
+
             ?>
             <div class="sp-med w-full lt-shade">
                 <div class="top txt-fl sm-med-b">
-                    <span>Classname - Subject</span>
+                    <span><?= $class["program_name"] ?> - <?= $class["course_name"] ?></span>
                 </div>
                 <div class="middle flex flex-column">
-                    <span><?= $total ?> Students</span>
-                    <span>Average Grade: <?= $grade ?></span>
+                    <span><?= fetchData1("COUNT(indexNumber) as total","students_table","program_id=".$class["program_id"])["total"] ?> Students</span>
+                    <span>Average Grade: <?php 
+                        $average = fetchData1("AVG(mark) as averageMark","results","course_id=".$class["course_id"]." AND teacher_id=".$teacher["teacher_id"])["averageMark"];
+                        echo giveGrade($average, $result_type)
+                    ?></span>
                 </div>
             </div>
-            <?php endfor; ?>
+            <?php endwhile;
+                else: ?>
+            <div class="sp-med w-full lt-shade p-lg txt-al-c">
+                <p>You have not been added to any class yet</p>
+            </div>
+            <?php endif; ?>
         </div>
     </div>
 
@@ -74,22 +107,31 @@
         </div>
         <div class="body flex flex-wrap txt-al-c m-sm">
             <?php 
-                $max = 5;
-                $mark = 95;
-                for($i = 1; $i <= $max; $i++):
-                    $mark = number_format((rand(70,$mark)), 1);
-                    $grade = giveGrade($mark);
+                $sql = "SELECT CONCAT(s.Lastname, ' ', s.Othernames) as student_name, p.program_name, AVG(r.mark) as averageMark
+                    FROM students_table s JOIN results r ON s.indexNumber=r.indexNumber
+                    JOIN program p ON p.program_id=s.program_id
+                    JOIN teachers t ON t.school_id=p.school_id
+                    WHERE '".implode(" ",$program_ids)."' LIKE CONCAT('%',s.program_id,' ','%')
+                    GROUP BY s.indexNumber ORDER BY averageMark DESC LIMIT 5
+                ";
+                $query = $connect2->query($sql);
+                if($query->num_rows > 0) :
+                    while($student = $query->fetch_assoc()) :
             ?>
             <div class="sp-med w-full lt-shade">
                 <div class="top flex flex-column sm-sm-b">
-                    <span>Student Name</span>
-                    <span class="txt-fs">- Class Name -</span>
+                    <span><?= ucwords($student["student_name"]) ?></span>
+                    <span class="txt-fs">- <?= $student["program_name"] ?> -</span>
                 </div>
                 <div class="middle txt-fs">
-                    <span>Grade <?= $grade ?> [<?= $mark ?>%]</span>
+                    <span>Grade <?= giveGrade($student["averageMark"], $result_type) ?> [<?= $student["averageMark"] ?>%]</span>
                 </div>
             </div>
-            <?php endfor; ?>
+            <?php endwhile; else: ?>
+            <div class="sp-med w-full lt-shade p-lg txt-al-c">
+                <p>No student data to be displayed yet</p>
+            </div>
+            <?php endif;  ?>
         </div>
     </div>
 </section>
@@ -99,14 +141,26 @@
         <h2>Announcements</h2>
     </div>
     <div class="body sm-med-t m-lg-tp wmax-md sm-auto">
-        <div class="sp-med w-full lt-shade-h">
-            <h3 class="top">Announcement Head</h3>
-            <div class="middle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur distinctio nemo ullam ea impedit quod provident et cumque, necessitatibus magni doloremque corrupti. Consectetur a soluta quos modi corporis sit voluptatibus!</div>
-        </div>
+        <?php 
+            $sql = "SELECT heading, body, date 
+                FROM announcement WHERE school_id = {$teacher['school_id']} AND audience IN ('teachers', 'all')";
+            $query = $connect2->query($sql);
 
-        <div class="sp-med w-full lt-shade">
+            if($query->num_rows > 0) :
+                while($message = $query->fetch_assoc()) :
+        ?>
+        <div class="sp-med w-full lt-shade-h">
+            <h3 class="top"><?= $message["heading"] ?></h3>
+            <div class="middle sm-med-tp"><?= $message["body"] ?></div>
+            <div class="foot">
+                <span class="txt-fs color-dark"><?= date("F d, Y", strtotime($message["date"])) ?></span>
+            </div>
+        </div>
+        <?php endwhile; else: ?>
+        <div class="sp-med w-full p-lg txt-al-c lt-shade">
             <div class="middle">No Announcements have been made</div>
         </div>
+        <?php endif; ?>
     </div>
 </section>
 
