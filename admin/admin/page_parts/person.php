ef51d520097b4564551be2ed42930ed1d17a2aa1
MatrixMe00 <safosah00@gmail.com>
2023-11-21 17:42:16 +0000

    Update person.php

--- admin/admin/page_parts/person.php
+++ admin/admin/page_parts/person.php
@@ -9,117 +9,141 @@
         <h3>Users on your System</h3>
     </div>
     <?php 
-        $result = $connect->query("SELECT a.*, s.schoolName, r.title AS roleTitle 
-        FROM admins_table a JOIN schools s
-        ON a.school_id = s.id
-        JOIN roles r
-        ON a.role = r.id 
-        WHERE s.id = $user_school_id AND r.access = TRUE AND a.username!='New User'") or die($connect->error);
+        $users = decimalIndexArray(fetchData(...[
+            "columns" => ["a.*", "s.schoolName", "r.title as roleTitle"],
+            "table" => [
+                [
+                    "join" => "admins_table schools",
+                    "alias" => "a s",
+                    "on" => "school_id id"
+                ],
+                [
+                    "join" => "admins_table roles",
+                    "alias" => "a r",
+                    "on" => "role id"
+                ]
+            ],
+            "where" => ["s.id=$user_school_id", "r.access < 3", "LOWER(a.username) != 'new user'"],
+            "where_binds" => "AND",
+            "limit" => 0
+        ]));
 
-        if($result->num_rows > 0){
+        if($users):
     ?>
     <div class="middle">
-        <?php while($row = $result->fetch_assoc()){?>
+        <?php foreach($users as $user): ?>
         <div class="user_container flex flex-column">
             <div class="top">
-                <h4><?php echo $row["fullname"] ?> (<span class="username"><?php echo $row["username"] ?></span>)</h4>
+                <h4><?= $user["fullname"] ?> (<span class="username"><?= $user["username"] ?></span>)</h4>
             </div>
             <div class="desc flex flex-wrap">
-                <?php if($row["schoolName"] != null){ ?>
+                <?php if($user["schoolName"] != null): ?>
                 <div class="school_name">
-                    <span><?php echo $row["schoolName"] ?></span>
+                    <span><?= $user["schoolName"] ?></span>
                 </div>
-                <?php } ?>
+                <?php endif; ?>
                 <div class="role">
-                    <span><?php echo formatName($row["roleTitle"])?></span>
+                    <span><?= formatName($user["roleTitle"])?></span>
                 </div>
                 <div class="member_since">
-                    <span>Added on <?php echo date("jS F, Y", strtotime($row["adYear"]))?></span>
+                    <span>Added on <?= date("jS F, Y", strtotime($user["adYear"]))?></span>
                 </div>
             </div>
             <div class="foot">
-                <?php if($user_id == $row["user_id"]){ ?>
+                <?php if($user_id == $user["user_id"]): ?>
                 <span class="item-event edit">Edit</span>
-                <?php } ?>
-                <?php if($user_details["role"] <= 4){ ?>
-                <span class="item-event status" data-user-id="<?php echo $row["user_id"]?>"><?php 
-                    if($row["Active"] == true && $user_id != $row["user_id"]){
+                <?php endif; ?>
+                <?php if($user_details["role"] <= 4): ?>
+                <span class="item-event status" data-user-id="<?= $user["user_id"]?>"><?php 
+                    if($user["Active"] == true && $user_id != $user["user_id"]){
                         echo "Deactivate";
-                    }elseif($user_id != $row["user_id"]){
+                    }elseif($user_id != $user["user_id"]){
                         echo "Activate";
                     }
                 ?></span>
-                <?php } ?>
-                <?php if($row["user_id"] != $user_id && $user_details["role"] == 3){?>
-                <span class="item-event delete" data-user-id="<?php echo $row["user_id"]?>">Delete</span>
-                <?php }?>
+                <?php endif; ?>
+                <?php if($user["user_id"] != $user_id && $admin_access == 2): ?>
+                <span class="item-event delete" data-user-id="<?= $user["user_id"]?>">Delete</span>
+                <?php endif; ?>
             </div>
         </div>
-        <?php } ?>
+        <?php endforeach; ?>
     </div>
-    <?php }else{?>
+    <?php else:?>
     <div class="body empty" style="margin-top: 10px">
         <p>No users were found in the system</p>
     </div>
-    <?php }?>
+    <?php endif; ?>
 </section>
 
 <?php 
-    $result = $connect->query("SELECT a.*, s.schoolName, r.title AS roleTitle 
-        FROM admins_table a JOIN schools s
-        ON a.school_id = s.id
-        JOIN roles r
-        ON a.role = r.id 
-        WHERE s.id = $user_school_id AND r.access = TRUE AND a.username='New User'") or die($connect->error);
+    $users = decimalIndexArray(fetchData(...[
+        "columns" => ["a.*", "s.schoolName", "r.title as roleTitle"],
+        "table" => [
+            [
+                "join" => "admins_table schools",
+                "alias" => "a s",
+                "on" => "school_id id"
+            ],
+            [
+                "join" => "admins_table roles",
+                "alias" => "a r",
+                "on" => "role id"
+            ]
+        ],
+        "where" => ["s.id=$user_school_id", "r.access < 3", "LOWER(a.username) = 'new user'"],
+        "where_binds" => "AND",
+        "limit" => 0
+    ]));
 
-    if($result->num_rows > 0){
+    if($users):
 ?>
 <section class="page_setup" id="users">
     <div class="head">
         <h3>Users yet to register on your System</h3>
     </div>
     <div class="middle">
-        <?php while($row = $result->fetch_assoc()){?>
+        <?php foreach($users as $user):?>
         <div class="user_container flex flex-column">
             <div class="top">
-                <h4><?php echo $row["fullname"] ?></h4>
+                <h4><?= $user["fullname"] ?></h4>
             </div>
             <div class="desc flex flex-wrap">
-                <?php if($row["schoolName"] != null){ ?>
+                <?php if($user["schoolName"] != null): ?>
                 <div class="school_name">
-                    <span><?php echo $row["schoolName"] ?></span>
+                    <span><?= $user["schoolName"] ?></span>
                 </div>
-                <?php } ?>
+                <?php endif; ?>
                 <div class="role">
-                    <span><?php echo formatName($row["roleTitle"])?></span>
+                    <span><?= formatName($user["roleTitle"]) ?></span>
                 </div>
                 <div class="member_since">
-                    <span>Added on <?php echo date("jS F, Y", strtotime($row["adYear"]))?></span>
+                    <span>Added on <?= date("jS F, Y", strtotime($user["adYear"]))?></span>
                 </div>
             </div>
             <div class="foot">
-                <?php if($user_id == $row["user_id"]){ ?>
+                <?php if($user_id == $user["user_id"]): ?>
                 <span class="item-event edit">Edit</span>
-                <?php } ?>
-                <span class="item-event status" data-user-id="<?php echo $row["user_id"]?>">
+                <?php endif; ?>
+                <span class="item-event status" data-user-id="<?= $user["user_id"]?>">
                 <?php 
-                    if($row["Active"] == true && $user_id != $row["user_id"]){
+                    if($user["Active"] == true && $user_id != $user["user_id"]){
                         echo "Deactivate";
-                    }elseif($user_id != $row["user_id"]){
+                    }elseif($user_id != $user["user_id"]){
                         echo "Activate";
                     }
                 ?></span>
-                <?php if($row["user_id"] != $user_id){?>
-                <span class="item-event delete" data-user-id="<?php echo $row["user_id"]?>">Delete</span>
-                <?php }?>
+                <?php if($user["user_id"] != $user_id): ?>
+                <span class="item-event delete" data-user-id="<?= $user["user_id"]?>">Delete</span>
+                <?php endif; ?>
             </div>
         </div>
-        <?php } ?>
+        <?php endforeach; ?>
     </div>
 </section>
-<?php } ?>
+<?php endif; ?>
 
-<?php if($user_details["role"] == 3){ ?>
+<?php if($admin_access === 2): ?>
 <section>
     <div class="base-foot">
         <div class="btn w-full w-full-child" style="max-width: 12rem">
@@ -127,10 +151,10 @@
         </div>
     </div>
 </section>
-<?php } ?>
+<?php endif; ?>
 
 <div id="editAccount" class="fixed flex flex-center-content flex-center-align form_modal_box no_disp">
-<form action="<?php echo $url?>/admin/submit.php" method="post" class="" name="user_account_form">
+    <form action="<?= $url?>/admin/submit.php" method="post" class="" name="user_account_form">
         <div class="head">
             <h2>My Account</h2>
         </div>
@@ -142,31 +166,31 @@
             <div class="joint">
                 <label for="fullname">
                     <span class="label_image">
-                        <img src="<?php echo $url?>/assets/images/icons/id card.png" alt="fullname">
+                        <img src="<?= $url?>/assets/images/icons/id card.png" alt="fullname">
                     </span>
                     <input type="text" name="fullname" id="fullname" class="text_input" placeholder="Full Name" autocomplete="off"
-                    title="Enter your full name" value="<?php echo $user_details["fullname"] ?>">
+                    title="Enter your full name" value="<?= $user_details["fullname"] ?>">
                 </label>
                 <label for="email">
                     <span class="label_image">
-                        <img src="<?php echo $url?>/assets/images/icons/mail-outline.svg" alt="email">
+                        <img src="<?= $url?>/assets/images/icons/mail-outline.svg" alt="email">
                     </span>
                     <input type="email" name="email" id="email" class="text_input" placeholder="Email Address" autocomplete="off"
-                    title="Provide your email address" value="<?php echo $user_details["email"] ?>">
+                    title="Provide your email address" value="<?= $user_details["email"] ?>">
                 </label>
                 <label for="contact">
                     <span class="label_image">
-                        <img src="<?php echo $url?>/assets/images/icons/phone-portrait-outline.svg" alt="contact">
+                        <img src="<?= $url?>/assets/images/icons/phone-portrait-outline.svg" alt="contact">
                     </span>
                     <input type="text" name="contact" id="contact" class="text_input tel" placeholder="Provide your contact" autocomplete="off"
-                    value="<?php echo $user_details["contact"] ?>">
+                    value="<?= $user_details["contact"] ?>">
                 </label>
                 <label for="username">
                     <span class="label_image">
-                        <img src="<?php echo $url?>/assets/images/icons/person-outline.svg" alt="username">
+                        <img src="<?= $url?>/assets/images/icons/person-outline.svg" alt="username">
                     </span>
                     <input type="text" name="username" id="username" class="text_input" placeholder="Enter your username" minlength="5" 
-                    autocomplete="off" value="<?php echo $user_details["username"] ?>">
+                    autocomplete="off" value="<?= $user_details["username"] ?>">
                 </label>
             </div>
             <div class="flex">
@@ -181,11 +205,11 @@
     </form>
 </div>
     
-    <?php if($user_details["role"] == 3){ ?>
-    <div id="adminAdd" class="fixed flex flex-center-content flex-center-align form_modal_box">
+    <?php if($admin_access === 2): ?>
+    <div id="adminAdd" class="fixed flex flex-center-content flex-center-align form_modal_box no_disp">
         <?php require_once($rootPath."/admin/adminAdd.php") ?>
     </div>
-    <?php } ?>
+    <?php endif; ?>
 
-    <script type="text/javascript" src="<?php echo $url?>/admin/assets/scripts/person.min.js?v=<?php echo time()?>"></script>
-    <script src="<?php echo $url?>/assets/scripts/form/general.min.js?v=<?php echo time()?>"></script>
+    <script type="text/javascript" src="<?= $url?>/admin/assets/scripts/person.min.js?v=<?= time()?>"></script>
+    <script src="<?= $url?>/assets/scripts/form/general.min.js?v=<?= time()?>"></script>
