3af0d137f78ec8c70ddaa3c50092cd1fa1522e07
MatrixMe <safosah00@gmail.com>
2024-06-13 01:11:44 +0000

    Update deactivate_access.php

--- cron/deactivate_access.php
+++ cron/deactivate_access.php
@@ -1,5 +1,7 @@
 <?php 
+    require "session.php";
+
     $today = date('Y-m-d H:i:s');
     $sql = "UPDATE accesstable SET status=0 WHERE expiryDate < STR_TO_DATE('$today', '%Y-%m-%d %H:%i:%s')";
-    $connect2->query($sql)
+    echo $connect2->query($sql);
 ?>
